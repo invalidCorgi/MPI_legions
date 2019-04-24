eff5265958327886eec5e49313b55eee8ff73e52
Johny <j.dudziak97@gmail.com>
2019-04-24 20:53:24 +0200

    Threads added, mutex fixed

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#define T 10
+#define T 1
 #define MSG_WANT 1
 #define MSG_WANT_SIZE 2
 #define MSG_RELEASE 2
@@ -77,20 +77,27 @@ void *RecvMessages(void *arg) {
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
+    
+    pthread_t thread_id;
+    errno = pthread_create(&thread_id, NULL, RecvMessages, NULL);
+
     for (int &road_capacity : roads_capacity) {
-        road_capacity = 200 + rand() % 500;
+        road_capacity = 200 + rand() % 50;
+        printf("Droga ma rozmiar %d.\n", road_capacity);
     }
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     srand(static_cast<unsigned int>(rank * 100));
-    legion_size = 50 + rand() % 100;
+    legion_size = 100 + rand() % 100;
+    printf("Legion %d ma rozmiar %d.\n",rank ,legion_size);
     MPI_Comm_size(MPI_COMM_WORLD, &L);
+    pthread_mutex_unlock(&lock);
     while(1) {
         int sleepTime = 3 + rand() % 5;
-        printf("%d: Rozkladamy oboz na %d sekund\n", rank, sleepTime);
+        //printf("%d: Rozkladamy oboz na %d sekund\n", rank, sleepTime);
         sleep(sleepTime);
         want_road_id = rand()%T;
         printf("%d: Otrzymalem rozkaz przemieszczenia traktem %d\n", rank, want_road_id);
-        printf("%d: Pytam sie innych czy moge wejsc na trakt\n", rank);
+        //printf("%d: Pytam sie innych czy moge wejsc na trakt\n", rank);
         my_clock++;
         want_road_clock = my_clock;
         permissions_to_go = L - 1;
@@ -98,17 +105,19 @@ int main(int argc, char **argv) {
         msg_send[1] = want_road_id;
         want_road_clock = msg_send[0];
         //MPI_Bcast(msg_send, MSG_WANT_SIZE, MPI_INT, rank, MPI_COMM_WORLD);
+        pthread_mutex_lock(&lock);
         for(int i = 0; i < L; i++){
             if(i != rank){
                 MPI_Send(msg_send, MSG_WANT_SIZE, MPI_INT, i, MSG_WANT, MPI_COMM_WORLD);
             }
         }
-        printf("%d: Czekam na pozwolenia\n", rank);
+        //printf("%d: Czekam na pozwolenia\n", rank);
         pthread_mutex_lock(&lock);
         sleepTime = 3 + rand() % 5;
-        printf("%d: Wchodze na trakt na %d sekund\n", rank, sleepTime);
+        printf("%d: Wchodze na %d trakt na %d sekund\n", rank, want_road_id, sleepTime);
         sleep(sleepTime);
-        printf("%d: Wychodze z traktu i informuje o tym zainteresowanych\n", rank);
+        pthread_mutex_unlock(&lock);
+        printf("%d: Wychodze z %d traktu i informuje o tym zainteresowanych\n", rank, want_road_id);
         msg_send[0] = my_clock;
         msg_send[1] = want_road_id;
         msg_send[2] = legion_size;
@@ -119,5 +128,6 @@ int main(int argc, char **argv) {
             legions_id_wanted_same_road.pop_back();
         }
     }
+    errno = pthread_join(thread_id, NULL);
     return 0;
 }
