686e80ad058bf8e8b423867ae5208b9954400415
invalidcorgi <invalidcorgi@gmail.com>
2019-05-13 12:18:53 +0200

    less synchronization, solved release problem

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #define MSG_WANT 1
 #define MSG_WANT_SIZE 2
 #define MSG_RELEASE 2
-#define MSG_RELEASE_SIZE 3
+#define MSG_RELEASE_SIZE 4
 #define MSG_PERMISSION 3
 #define MSG_PERMISSION_SIZE 2
-#define MSG_MAX_SIZE 3
+#define MSG_MAX_SIZE 4
 
 #include <pthread.h>
 #include "mpi.h"
@@ -18,7 +18,7 @@ int L, rank, legion_size;
 int want_road_id = -1;
 int my_clock, want_road_clock, people_already_in_want_road = 0;
 int roads_capacity[T];
-std::vector<unsigned int> legions_id_wanted_same_road;
+std::vector<int> legions_id_wanted_same_road;
 int permissions_to_go;
 pthread_mutex_t lock;
 pthread_mutex_t releaseLock;
@@ -35,10 +35,10 @@ bool can_i_enter_critical_section(){
 void *RecvMessages(void *arg) {
     while (1) {
         MPI_Recv(msg_recv, MSG_MAX_SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        pthread_mutex_lock(&clockLock);
         int sender_clock = msg_recv[0];
-        msg_recv[0] = my_clock;
         int sender_id = status.MPI_SOURCE;
+        pthread_mutex_lock(&clockLock);
+        msg_recv[0] = my_clock;
         my_clock = std::max(my_clock, sender_clock) + 1;
         pthread_mutex_unlock(&clockLock);
         switch (status.MPI_TAG) {
@@ -49,6 +49,7 @@ void *RecvMessages(void *arg) {
                     pthread_mutex_lock(&clockLock);
                     if ((want_road_clock < sender_clock) || (want_road_clock == sender_clock && rank < sender_id)) {
                         legions_id_wanted_same_road.push_back(sender_id);
+                        legions_id_wanted_same_road.push_back(sender_clock);
                         msg_recv[1] = legion_size;
                         MPI_Send(msg_recv, MSG_PERMISSION_SIZE, MPI_INT, sender_id, MSG_PERMISSION, MPI_COMM_WORLD);
                     }else{
@@ -66,7 +67,8 @@ void *RecvMessages(void *arg) {
             case MSG_RELEASE: {
                 int road_id = msg_recv[1];
                 int other_legion_size = msg_recv[2];
-                if(want_road_id == road_id) {
+                int want_road_clock_from_sender = msg_recv[3];
+                if(want_road_id == road_id && want_road_clock == want_road_clock_from_sender) {
                     people_already_in_want_road -= other_legion_size;
                     if(can_i_enter_critical_section()){
                         pthread_mutex_unlock(&lock);
@@ -113,9 +115,10 @@ int main(int argc, char **argv) {
         pthread_mutex_lock(&clockLock);
         my_clock++;
         want_road_clock = my_clock;
-        msg_send[0] = want_road_clock;
         pthread_mutex_unlock(&clockLock);
+        people_already_in_want_road = 0;
         permissions_to_go = L - 1;
+        msg_send[0] = want_road_clock;
         msg_send[1] = want_road_id;
         pthread_mutex_lock(&lock);
         for(int i = 0; i < L; i++){
@@ -136,9 +139,10 @@ int main(int argc, char **argv) {
         msg_send[1] = want_road_id;
         msg_send[2] = legion_size;
         want_road_id = -1;
-        people_already_in_want_road = 0;
         pthread_mutex_lock(&releaseLock);
         while (legions_id_wanted_same_road.size() > 0){
+            msg_send[3] = legions_id_wanted_same_road.back();
+            legions_id_wanted_same_road.pop_back();
             MPI_Send(msg_send, MSG_RELEASE_SIZE, MPI_INT, legions_id_wanted_same_road.back(), MSG_RELEASE, MPI_COMM_WORLD);
             legions_id_wanted_same_road.pop_back();
         }
